1d788db1b6ad02078b394f40a096d42263c08690
Mery <mceballosm04@iesalbarregas.es>
2025-04-22 17:51:19 +0200

    enemigos

--- juego/src/main/java/com/eve/model/Enemigo.java
+++ juego/src/main/java/com/eve/model/Enemigo.java
@@ -2,4 +2,23 @@ package com.eve.model;
 
 public class Enemigo {
 
+private int percepcion;
+
+public Enemigo(int puntosvida, int danio, int fuerza, int defensa, int velocidad, int percepcion) {
+    super(puntosvida, danio, fuerza, defensa, velocidad);
+    this.percepcion = percepcion;
 }
+
+
+    public int getPercepcion() {
+        return this.percepcion;
+    }
+
+    public void setPercepcion(int percepcion) {
+        this.percepcion = percepcion;
+    }
+
+
+}
+
+
--- juego/src/main/java/com/eve/model/Protagonista.java
+++ juego/src/main/java/com/eve/model/Protagonista.java
@@ -2,4 +2,6 @@ package com.eve.model;
 
 public class Protagonista {
 
+
+    
 }
