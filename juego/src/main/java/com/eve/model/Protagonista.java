60a0f1cf7276b7a10ac1ed1d3b821e44f3c1c667
eeveenax <eveliagilparedes@gmail.com>
2025-04-22 17:55:57 +0200

    personaje prota

--- juego/src/main/java/com/eve/model/Protagonista.java
+++ juego/src/main/java/com/eve/model/Protagonista.java
@@ -6,16 +6,15 @@ public class Protagonista extends Personaje {
     private int nivel;
     private int muertes;
 
-   
+    public Protagonista(int puntosvida, int danio, int fuerza, int defensa, int velocidad) {
+        super(puntosvida, danio, fuerza, defensa, velocidad);
+        this.nombre = nombre;
+        this.muertes = 0;
+        this.nivel = 1;
+    }
 
     public void matar() {
 
     }
 
-    public void subirNivl() {
-
-    }
-
-
-    
 }
