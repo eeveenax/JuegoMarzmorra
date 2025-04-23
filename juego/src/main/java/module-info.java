c26c9eea68b0542aa1bf7d5e593d8d3a3d372daa
eve <eveliagilparedes@gmail.com>
2025-04-23 11:57:51 +0200

    cambios escenas

--- juego/src/main/java/module-info.java
+++ juego/src/main/java/module-info.java
@@ -1,10 +1,9 @@
 module com.eve {
     requires javafx.controls;
     requires javafx.fxml;
-    requires transitive javafx.graphics;
+    requires javafx.graphics;
 
     opens com.eve to javafx.fxml;
-
     opens com.eve.controllers to javafx.fxml;
 
     exports com.eve;
